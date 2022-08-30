f9b9a9fa3d775dde6ec11de6d1f139f5ff99e83b
evan_glb <evan@qq.com>
2022-08-31 00:24:55 +0800

    秒杀测试

---
+++ src/main/java/com/zap/DiscountApplication.java
@@ -0,0 +1,18 @@
+package com.zap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: DiscountApplication
+ * @Description:
+ * @author: Apeng
+ * @date: 2022/8/31 0:19
+ */
+
+@SpringBootApplication
+public class DiscountApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountApplication.class,args);
+    }
+}
